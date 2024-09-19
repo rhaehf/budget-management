86f78f7f64ef66252c33ddd14e5fcc91907f4327
Jinhui <jda0526@naver.com>
2024-09-20 00:43:33 +0900

    feat : BaseException 메서드 추가 및 BadRequestException 구현 → #1

--- src/main/java/com/management/budget/exception/BaseException.java
+++ src/main/java/com/management/budget/exception/BaseException.java
@@ -1,15 +1,17 @@
 package com.management.budget.exception;
 
 import lombok.Getter;
-import lombok.RequiredArgsConstructor;
 
 @Getter
-@RequiredArgsConstructor
 public class BaseException extends RuntimeException {
     private final ErrorCode errorCode;
-    private final String message;
 
     public BaseException(ErrorCode errorCode) {
-        this(errorCode, errorCode.getMessage());
+        this(errorCode, errorCode.getMessage()); // 밑에 생성자를 호출
+    }
+
+    public BaseException(ErrorCode errorCode, String message) {
+        super(message); // 부모 클래스 RuntimeException의 message 필드를 설정
+        this.errorCode = errorCode;
     }
 }
